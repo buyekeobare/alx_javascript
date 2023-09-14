cb63fab4b2108365d76938b74a02a611aa6a4709
buyekeobare <buyekeobare@gmail.com>
2023-09-14 21:20:59 +0300

    4-completed_tasks.js

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/**
+    a script that computes the number of tasks completed by user id.
+
+The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+Only print users with completed task
+You must use the module request
+ */
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
